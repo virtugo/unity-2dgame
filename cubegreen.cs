558bd9911dc12dae56e7178cf92f9d2a9bcdfa29
virtugo <virtugo@ya.ru>
2014-09-07 19:16:16 +0400

    Update cubegreen.cs

--- cubegreen.cs
+++ cubegreen.cs
@@ -3,18 +3,36 @@ using System.Collections;
 
 public class cubegreen1 : MonoBehaviour {
 
+	public Transform trCubegreen;
+	bool cubeclicked = false;
+	bool flag = true;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// сгенерировать новые квадраты
+		if (cubeclicked) {
+			// сгенерировать ребенка
+			Transform t = Instantiate(trCubegreen) as Transform;
+			t.parent = transform;
+			t.localPosition = Vector3.zero;
+			t.localPosition = new Vector3(0,1,0);
+			t.gameObject.name = "CubeGreenX";
+			flag = false;
+			cubeclicked = false;
+		}
 	}
 
 	void OnMouseDown() {
-		Debug.Log("cubegreen: clicked");
+		if (flag) {
+			cubeclicked = true;
+			Debug.Log ("cubegreenX: clicked");
+		}
+		else Debug.Log("cubegreenX: already clicked");
 	}
 
 	void OnMouseEnter(){  

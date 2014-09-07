29b47aef39010695ba95663b71681024f73485b0
virtugo <virtugo@ya.ru>
2014-09-07 18:12:34 +0400

    Update cubered.cs

--- cubered.cs
+++ cubered.cs
@@ -4,32 +4,35 @@ using System.Collections;
 public class cubered : MonoBehaviour {
 	
 	public Transform trCubegreen;
-	bool flag = false;
+	bool cubeclicked = false;
+	bool flag = true;
 
 	// Use this for initialization
 	void Start () {
-		Transform t = Instantiate(trCubegreen) as Transform;
-		t.parent = transform;
-		t.localPosition = Vector3.zero;
-		t.localPosition = new Vector3(0,1,0);
-		t.gameObject.name = "CubeGreenX";
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// сгенерировать новые квадраты
-		if (flag){
-			if (aaa.cubechecked) {
-				// включить детишек
-			}
+		if (cubeclicked) {
+			// сгенерировать ребенка
+			Transform t = Instantiate(trCubegreen) as Transform;
+			t.parent = transform;
+			t.localPosition = Vector3.zero;
+			t.localPosition = new Vector3(0,1,0);
+			t.gameObject.name = "CubeGreenX";
 			flag = false;
+			cubeclicked = false;
 		}
 	}
 
 	void OnMouseDown() {
-		Debug.Log("cubered: mouse down");
-		aaa.cubeclicked = true;
-		flag = true;
+		if (flag) {
+			cubeclicked = true;
+			Debug.Log ("cubered: clicked");
+		}
+		else Debug.Log("cubered: already clicked");
 	}
 
 	void OnMouseEnter(){  

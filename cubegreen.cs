a6683b2cf7c78e87330ab6e4d78a4d5565a8d54c
virtugo <virtugo@ya.ru>
2014-09-07 23:53:40 +0400

    Update cubegreen.cs

--- cubegreen.cs
+++ cubegreen.cs
@@ -8,11 +8,13 @@ public class cubegreen1 : MonoBehaviour {
 	bool flag = true;
 	bool flag2 = false;
 	float a = 0;
+	int i = 0;
 
 	// Use this for initialization
 	void Start () {
 		Random.seed = (int)System.DateTime.Now.Ticks;
-		// добавить обработку тупика, иначе зависнет
+		//if((aaa.intX>0)&&(aaa.intX<19)&&(aaa.intY>0)&&(aaa.intY<8)){
+		//}
 		do{
 			a = Random.Range (1,5);
 
@@ -36,38 +38,43 @@ public class cubegreen1 : MonoBehaviour {
 					if(aaa.arrWorld[aaa.intX-1,aaa.intY]==0)flag2 = true;
 				}
 			}
-		}while(!flag2);
+			i++;
+			// обработка тупика, иначе зависнет
+			if(i=100) Debug.Log("cubegreen: random failed");
+		}while((!flag2)&&(i<100));
 		//Debug.Log ("random: " + a);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// сгенерировать новые квадраты
-		if (cubeclicked) {
-			Transform t = Instantiate(trCubegreen) as Transform;
-			t.parent = transform;
-			// сгенерировать ребенка
-			if (a==1){
-				aaa.intY--;
-				t.localPosition = new Vector3(0,1.1F,0);
+		if (flag2){
+			if (cubeclicked) {
+				Transform t = Instantiate(trCubegreen) as Transform;
+				t.parent = transform;
+				// сгенерировать ребенка
+				if (a==1){
+					aaa.intY--;
+					t.localPosition = new Vector3(0,1.1F,0);
+				}
+				else if(a==2){
+					aaa.intX++;
+					t.localPosition = new Vector3(1.1F,0,0);
+				}
+				else if(a==3){
+					aaa.intY++;
+					t.localPosition = new Vector3(0,-1.1F,0);
+				}
+				else if(a==4){
+					aaa.intX--;
+					t.localPosition = new Vector3(-1.1F,0,0);
+				}
+				t.gameObject.name = "CubeGreenX";
+				flag = false;
+				cubeclicked = false;
+				Debug.Log ("x=" + aaa.intX + ", y=" + aaa.intY);
+				aaa.arrWorld[aaa.intX,aaa.intY]=1;
 			}
-			else if(a==2){
-				aaa.intX++;
-				t.localPosition = new Vector3(1.1F,0,0);
-			}
-			else if(a==3){
-				aaa.intY++;
-				t.localPosition = new Vector3(0,-1.1F,0);
-			}
-			else if(a==4){
-				aaa.intX--;
-				t.localPosition = new Vector3(-1.1F,0,0);
-			}
-			t.gameObject.name = "CubeGreenX";
-			flag = false;
-			cubeclicked = false;
-			Debug.Log ("x=" + aaa.intX + ", y=" + aaa.intY);
-			aaa.arrWorld[aaa.intX,aaa.intY]=1;
 		}
 	}
 

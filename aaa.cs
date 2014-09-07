deaa18b82a069cc8d61a140e26a32657201217fc
virtugo <virtugo@ya.ru>
2014-09-08 00:03:15 +0400

    Update aaa.cs
    maxX, maxY

--- aaa.cs
+++ aaa.cs
@@ -6,15 +6,17 @@ public class aaa : MonoBehaviour {
 	public static int[,] arrWorld;
 	public static int intX = 10;
 	public static int intY = 4;
+	public static int maxX = 19;
+	public static int maxY = 8;
 	int i=0;
 	int j=0;
 
 	// Use this for initialization
 	void Start () {
-		arrWorld = new int[20,9];
+		arrWorld = new int[(maxX+1),(maxY+1)];
 		// обнуляем массив
-		for(i=0;i<20;i++){
-			for(j=0;j<9;j++){
+		for(i=0;i<=maxX;i++){
+			for(j=0;j<=maxY;j++){
 				arrWorld[i,j] = 0;
 			}
 		}

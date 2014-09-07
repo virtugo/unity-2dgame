1e20060450125a0f99cec60e84e9ef7a1646398d
virtugo <virtugo@ya.ru>
2014-09-07 23:09:41 +0400

    Update aaa.cs

--- aaa.cs
+++ aaa.cs
@@ -5,16 +5,16 @@ public class aaa : MonoBehaviour {
 
 	public static int[,] arrWorld;
 	public static int intX = 10;
-	public static int intY = 10;
+	public static int intY = 4;
 	int i=0;
 	int j=0;
 
 	// Use this for initialization
 	void Start () {
-		arrWorld = new int[20,20];
+		arrWorld = new int[20,9];
 		// обнуляем массив
 		for(i=0;i<20;i++){
-			for(j=0;j<20;j++){
+			for(j=0;j<9;j++){
 				arrWorld[i,j] = 0;
 			}
 		}

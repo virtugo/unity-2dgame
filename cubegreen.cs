d3a13c78262815b0617db47ca7e72967a190f259
virtugo <virtugo@ya.ru>
2014-09-07 23:35:46 +0400

    Update cubegreen.cs

--- cubegreen.cs
+++ cubegreen.cs
@@ -28,63 +28,44 @@ public class cubegreen1 : MonoBehaviour {
 			}
 			else if (a==2){
 				if(aaa.intX<19){
-					if(aaa.arrWorld[aaa.intX+1,aaa.intY]==0) flag2 = true;
+					if(aaa.arrWorld[aaa.intX+1,aaa.intY]==0)flag2 = true;
 				}
 			}
 			else if (a==4){
 				if(aaa.intX>0){
-					if(aaa.arrWorld[aaa.intX-1,aaa.intY]==0) flag2 = true;
+					if(aaa.arrWorld[aaa.intX-1,aaa.intY]==0)flag2 = true;
 				}
 			}
 		}while(!flag2);
-		Debug.Log ("random: " + a);
+		//Debug.Log ("random: " + a);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// сгенерировать новые квадраты
 		if (cubeclicked) {
+			Transform t = Instantiate(trCubegreen) as Transform;
+			t.parent = transform;
 			// сгенерировать ребенка
 			if (a==1){
 				aaa.intY--;
-				Transform t = Instantiate(trCubegreen) as Transform;
-				t.parent = transform;
-				t.localScale = new Vector3(1,1,1);
 				t.localPosition = new Vector3(0,1.1F,0);
-				t.gameObject.name = "CubeGreenX";
-				flag = false;
-				cubeclicked = false;
 			}
 			else if(a==2){
 				aaa.intX++;
-				Transform t = Instantiate(trCubegreen) as Transform;
-				t.parent = transform;
-				t.localPosition = Vector3.zero;
 				t.localPosition = new Vector3(1.1F,0,0);
-				t.gameObject.name = "CubeGreenX";
-				flag = false;
-				cubeclicked = false;
 			}
 			else if(a==3){
 				aaa.intY++;
-				Transform t = Instantiate(trCubegreen) as Transform;
-				t.parent = transform;
-				t.localPosition = Vector3.zero;
 				t.localPosition = new Vector3(0,-1.1F,0);
-				t.gameObject.name = "CubeGreenX";
-				flag = false;
-				cubeclicked = false;
 			}
 			else if(a==4){
 				aaa.intX--;
-				Transform t = Instantiate(trCubegreen) as Transform;
-				t.parent = transform;
-				t.localPosition = Vector3.zero;
 				t.localPosition = new Vector3(-1.1F,0,0);
-				t.gameObject.name = "CubeGreenX";
-				flag = false;
-				cubeclicked = false;
 			}
+			t.gameObject.name = "CubeGreenX";
+			flag = false;
+			cubeclicked = false;
 			Debug.Log ("x=" + aaa.intX + ", y=" + aaa.intY);
 			aaa.arrWorld[aaa.intX,aaa.intY]=1;
 		}
@@ -93,7 +74,7 @@ public class cubegreen1 : MonoBehaviour {
 	void OnMouseDown() {
 		if (flag) {
 			cubeclicked = true;
-			Debug.Log ("cubegreen: clicked");
+			//Debug.Log ("cubegreen: clicked");
 		}
 		else Debug.Log("cubegreen: already clicked");
 	}

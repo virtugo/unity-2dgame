5e8757d715fc2e73e70f6f11933e7eb234222ad5
virtugo <virtugo@ya.ru>
2014-09-08 16:19:43 +0400

    Update aaa.cs

--- aaa.cs
+++ aaa.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class aaa : MonoBehaviour {
 
+	public static int redcoordX = 7;
+	public static int redcoordY = 4;
+
 	public static int[,] arrWorld;
-	public static int intX = 10;
-	public static int intY = 4;
-	public static int maxX = 19;
+	public static int intX = redcoordX;
+	public static int intY = redcoordY;
+	public static int maxX = 14;
 	public static int maxY = 8;
 	int i=0;
 	int j=0;
@@ -20,7 +23,9 @@ public class aaa : MonoBehaviour {
 				arrWorld[i,j] = 0;
 			}
 		}
-		Debug.Log (arrWorld[0,0]);
+		// координата красного
+		arrWorld [redcoordX, redcoordY] = 1;
+		//Debug.Log (arrWorld[0,0]);
 	}
 	
 	// Update is called once per frame
